a331e3077dcb42a6dfe147714014bfa51bd26394
Ickyan <laperdin1785@gmail.com>
2024-11-06 19:42:20 +0200

    add blinking led with libopencm3

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <stdint.h>
-#include <stm32f10x.h>
+// #include <stm32f10x.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/timer.h>
+#include <libopencmsis/core_cm3.h>
+
+#define LCD_H 8
+#define LCD_W 128
 
 void delay_us(uint32_t us) {
 	__asm volatile (
@@ -17,6 +24,7 @@ void delay_us(uint32_t us) {
 	);
 }
 
+#if 0
 void SPI1_Write(uint8_t data){
 	while( SPI1->SR & SPI_SR_BSY); 
 	while( (SPI1->SR & SPI_SR_TXE) == 0);
@@ -165,5 +173,18 @@ int __attribute((noreturn)) main(void) {
 		}
 		// ToDo: delay_ms(1000)
     }
+
+}
+#endif
+
+int main(void){
+	rcc_clock_setup_pll(&rcc_hse_configs[RCC_CLOCK_HSE8_72MHZ]);
+	rcc_periph_clock_enable(RCC_GPIOC);
+	gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+
+	while(1){
+		gpio_toggle(GPIOC, GPIO13);
+		delay_us(100000);
+	}
 
 }
